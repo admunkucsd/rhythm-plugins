594e8b356cfb7c6b525f66e5795999f7e53623c3
Paul Botros <pbotros@ucsd.edu>
2023-05-10 16:19:40 -0700

    Add skipped tests

---
+++ Tests/RhythmPluginTests.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+
+
+//#include <ProcessorHeaders.h>
+
+#include <ModelProcessors.h>
+#include <ModelApplication.h>
+#include <TestFixtures.h>
+
+class RhythmPluginTests : public ProcessorTest {
+protected:
+    RhythmPluginTests() : ProcessorTest(1, 150) {
+    }
+
+    ~RhythmPluginTests() override {
+    }
+
+    void SetUp() override {
+        ProcessorTest::SetUp();
+    }
+
+    void TearDown() override {
+        ProcessorTest::TearDown();
+    }
+
+};
+
+TEST_F(RhythmPluginTests, DataIntegrityTest) {
+	GTEST_SKIP();
+}
+
+TEST_F(RhythmPluginTests, DataAlignmentTest) {
+	GTEST_SKIP();
+
+}
+TEST_F(RhythmPluginTests, ReadIndexTest) {
+    GTEST_SKIP();
+
+}
